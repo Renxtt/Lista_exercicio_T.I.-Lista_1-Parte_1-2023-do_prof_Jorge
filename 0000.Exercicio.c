506d8ce1d5be408ce52497caee5db5fe29d56d3b
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-03-16 14:28:55 -0300

    Add files via upload

---
+++ 0000.Exercicio.c
@@ -0,0 +1,32 @@
+/*
+
+1) Construa um programa que calcule a quantidade de latas de tinta necessárias e o custo para pintar tanques cilíndricos de combustível, onde são fornecidos a altura e o raio desse cilindro.
+Sabendo que:
+- a lata de tinta custa R$20,00
+- cada lata contém 5 litros
+cada litro de tinta pinta 3 metros quadrados.
+e que:
+Área do cilindro= 2 vezes a área da base + circunferencia da base vezes a altura
+e que raio e altura são dados de entrada.
+
+2) Construa um programa que tendo como entrada dois pontos quaisquer do plano P(x1,y1) e Q(x2,y2), imprima a distância entre eles. A fórmula da distância é: v(x2-x1)²+(y2-y1)2
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+main()
+{
+float x1, y1, x2, y2;
+float distancia;
+printf ("\nDigite o valor de x1: ");
+scanf("%f",&x1);
+printf ("\nDigite o valor de y1: ");
+scanf("%f",&y1); printf ("\nDigite o valor de x2: ");
+scanf("%f",&x2); printf ("\nDigite o valor de y2: ");
+scanf("%f",&y2);
+distancia-sqrt(pow((x2-x1),2)+pow((y2-y1),2)); printf ("\nA distancia entre os pontos P1 e P2 e' %.2f\n", distancia): system("PAUSE");
+}
+
+
+3)Infelismente eu não fui capaz achar
+
+*/
